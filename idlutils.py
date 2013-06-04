0bee30442f24489c563181f8312e5077a9286017
Scott Johnson <scottj@glasstowerstudios.com>
2013-06-04 16:35:57 -0500

    Issue #12: Added documentation for PrettyPrinter and classes within idlutils.

--- idlutils.py
+++ idlutils.py
@@ -1,19 +1,60 @@
 import re
 
+# @class IDLDescriptor A descriptor that may be found prefixing an attribute or
+#        method in an IDL interface.
+#
+# Examples of descriptors are things like "notxpcom" and "nostdcall". Typically,
+# they prefix a method or attribute in the following way in IDL:
+# [notxpcom] long getSomeValue();
+#
+# This class also holds a list of all the current descriptors loaded, so that
+# the list can be searched quickly. This list is a singleton (i.e. static)
+# across all IDLDescriptor objects.
 class IDLDescriptor:
   # Static list of IDL descriptors
   kDescriptorList = []
 
+  # Create a new IDLDescriptor object. Each IDLDescriptor contains two parts
+  # a token, which identifies what the IDL code actually looks like that invokes
+  # this descriptor, and a boolean indicating whether the descriptor causes
+  # binary compatibility to change (thus requiring a new IID for the interface).
+  #
+  # @param aToken A string containing the name of the IDLDescriptor, the token
+  #        that is used to invoke the descriptor.
+  # @param aAffectsBinaryCompat True, if adding or removing this IDLDescriptor
+  #        in an interface affects binary compatibility; False, otherwise.
+  # @note There is no method of changing an IDLDescriptor object's members after
+  #       having constructed it. This is intentional, as IDL descriptors cannot
+  #       change from affecting binary compatibility to not affecting binary
+  #       compatibility. Similarly, the token names cannot change, as this would
+  #       incentivize reuse of IDLDescriptor objects - something that should
+  #       be avoided, given how cheap they are to create.
   def __init__(self, aToken, aAffectsBinaryCompat):
     self.mToken = aToken
     self.mBinaryCompat = aAffectsBinaryCompat
 
+  # Return whether or not this IDLDescriptor affects binary compatibility.
+  #
+  # @returns True, if this IDLDescriptor object represents a descriptor that
+  #          affects binary compatibility; False otherwise.
   def affectsBinaryCompatibility(self):
     return self.mBinaryCompat
 
+  # Retrieve the token, or identifier, of the IDLDescriptor object.
+  #
+  # @returns A string representing the token, or identifier, of the descriptor.
   def getToken(self):
     return self.mToken
 
+  # Determine whether or not the IDL descriptor this object represents appears
+  # on a given line.
+  #
+  # @param aLine The line to check, presumably from an IDL file or patch file.
+  # @param aPrinter An optional argument of type PrettyPrinter to route debug
+  #        output from this method through.
+  #
+  # @returns True, if the idl descriptor which this object represents appears in
+  #          aLine; False, otherwise.
   def isInLine (self, aLine, aPrinter=None):
     match = re.search("^(\s)*[\+\-](\s)*\[(.*)](.*)", aLine)
     if match:
@@ -25,12 +66,41 @@ class IDLDescriptor:
           return True
     return False
 
+  # Determine if there are any known IDL descriptors in a given line.
+  #
+  # This method checks the given parameter against all known IDLDescriptor
+  # objects (all objects in the list kDescriptorList).
+  #
+  # @param aLine The line to check, presumably from an IDL file or patch file.
+  # @param aPrinter An optional argument of type PrettyPrinter to route debug
+  #        output from this method through.
+  #
+  # @returns True, if any known IDLDescriptor object in kDescriptorList appears
+  #          in aLine (checked by calling isInLine() repeatedly); False, if
+  #          isInLine(aLine) returns False for every IDLDescriptor in
+  #          kDescriptorList.
+  #
+  # @note This is a static function, so it should be called as:
+  #       IDLDescriptor.hasDescriptorsInLine(...)
   def hasDescriptorsInLine(aLine, aPrinter=None):
     for desc in IDLDescriptor.kDescriptorList:
       if desc.isInLine(aLine, aPrinter):
         return True
     return False
 
+  # Determine if any descriptors in a given line affect binary compatibility.
+  #
+  # @param aLIne The line to check, presumably from an IDL file or patch file.
+  # @param aPrinter An optional argument of type PrettyPrinter to route debug
+  #        output from this method through.
+  #
+  # @returns True, if hasDescriptorsInLine() returns true for aLine AND one of
+  #          the descriptors in kDescriptorList for which isInLine(aLine)
+  #          returns true also affects binary compatibility.
+  #
+  # @note This is a static function, so it should be called as:
+  #       IDLDescriptor.areDescriptorsInLineAffectingBinaryCompat(...)
+  # @todo This does not work correctly right now. See Issue # 14.
   def areDescriptorsInLineAffectingBinaryCompat(aLine, aPrinter=None):
     if not IDLDescriptor.hasDescriptorsInLine(aLine, aPrinter):
       return False
@@ -48,20 +118,46 @@ class IDLDescriptor:
   areDescriptorsInLineAffectingBinaryCompat = staticmethod(areDescriptorsInLineAffectingBinaryCompat)
   hasDescriptorsInLine = staticmethod(hasDescriptorsInLine)
 
+# @class SpecialBlockType An object representing a type of special block. A
+#        special block is a section of code that needs to be interpreted
+#        differently than other code. A comment block is an example of a special
+#        block.
+#
+#        Each special block type consists of two strings: the string token
+#        identifying the start of the block (e.g. "/*" for a comment block)
+#        and a string token identifying the end of the block (e.g. "*/" for a
+#        comment block).
 class SpecialBlockType:
+
+  # Create a new special block type with a given start and end token.
+  #
+  # @param aStartToken The string that indicates the start of the special block
+  #        of the new type.
+  # @param aEndToken The string that indicates the end of the special block of
+  #        the new type.
   def __init__(self, aStartToken, aEndToken):
     self.mStartToken = aStartToken
     self.mEndToken = aEndToken
 
+  # Retrieve the string representing the start token of the special block.
   def getStartToken(self):
     return self.mStartToken
 
+  # Retrieve the string representing the end token of the special block.
   def getEndToken(self):
     return self.mEndToken
 
+  # Convert this special block type to a string. This is used for debugging
+  # output, mostly.
   def __str__(self):
     return "[SpecialBlockType (" + str(self.mStartToken) + ")]"
 
+  # Compare one special block type to another.
+  #
+  # @param aOther Another object of type SpecialBlockType to compare to.
+  #
+  # @returns True, if and only if both the start token and the end token are
+  #          equal between this SpecialBlockType and aOther; False, otherwise.
   def __equals__(self, aOther):
     if self.mStartToken == aOther.mStartToken and self.mEndToken == aOther.mEndToken:
       return True
@@ -82,6 +178,13 @@ class SpecialBlockType:
       return True
     return False
 
+  # Determines whether a line contains a block of this SpecialBlockType
+  # completely within it.
+  #
+  # @param aLine The line to check.
+  #
+  # @returns True, if aLine contains both the start and end of this
+  #          SpecialBlockType; False, otherwise.
   def containsSpecialBlock(self, aLine):
     match = re.match("^(\s)*(.*)" + self.mStartToken + "(.*)" + self.mEndToken + "(\s)*(.*)", aLine)
     if match:
--- prettyprinter.py
+++ prettyprinter.py
@@ -1,5 +1,10 @@
 # This is a class that allows us to print prettier output to the command line.
+# It's designed so that you can create a single object of type PrettyPrinter,
+# then use that printer throughout your script.
 class PrettyPrinter:
+
+  # These are colors that can be modified to better suit tastes when using this
+  # class.
   INFO = '\033[92m'
   DEBUG = '\033[94m'
   WARNING = '\033[93m'
@@ -11,21 +16,39 @@ class PrettyPrinter:
     self.mDebugEnabled = aDebugEnabled
     self.mVerboseEnabled = aVerboseEnabled
 
+  # Print debug output to the console, if debug output is enabled, or do
+  # nothing. Most verbose.
   def debug(self, aMessage):
     self.printColor('debug', aMessage)
 
-  def warn(self, aMessage):
-    self.printColor('warn', aMessage)
-
-  def error(self, aMessage):
-    self.printColor('error', aMessage)
-
+  # Print informative output to the console, if info output is enabled, or do
+  # nothing. More verbose than warning output, but less than debug output.
   def info(self, aMessage):
     self.printColor('info', aMessage)
 
+  # Print warning output to the console, if warning output is enabled, or do
+  # nothing. More verbose than error output, but less than info output.
+  def warn(self, aMessage):
+    self.printColor('warn', aMessage)
+
+  # Print error output to the console. Error output is always enabled, so this
+  # will always print to the console. Least verbose output mechanism.
+  def error(self, aMessage):
+    self.printColor('error', aMessage)
+
+  # Determine if color printing is enabled or disabled.
+  #
+  # @returns True, if color output is disabled for this PrettyPrinter; False,
+  #          otherwise.
   def isColorDisabled(self):
     return not self.mColorEnabled
 
+  # Print output without color to the console using this PrettyPrinter object.
+  #
+  # @param aType The type of message to print - one of debug, info, warn, or
+  #        error.
+  # @param aMessage The string message to print. Each message is printed on a
+  #        separate line.
   def printNoColor(self, aType, aMessage):
     if aType == 'warn':
         print ("WARNING: " + str(aMessage))
@@ -41,6 +64,12 @@ class PrettyPrinter:
       # just print the message verbatim then, with no additions
       print(aMessage)
 
+  # Print output with color to the console using this PrettyPrinter object.
+  #
+  # @param aType The type of message to print - one of debug, info, warn, or
+  #        error.
+  # @param aMessage The string message to print. Each message is printed on a
+  #        separate line.
   def printColor(self, aType, aMessage):
     if self.isColorDisabled():
       self.printNoColor(aType, aMessage)
